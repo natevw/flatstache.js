a7f1d40a4b38306376c5b1d09457e9dc695569d7
Aaron McCall <aaron@agrinect.com>
2011-06-09 12:31:10 -0700

    renamed regexes to have meaningful names and added _val_or_empty() because if data[key] === 0 then  was being rendered.

--- flatstache.js
+++ flatstache.js
@@ -3,18 +3,21 @@
  (c) 2011 Nathan Vander Wilt and Aaron McCall. MIT license.
 */
 var Flatstache = (function(){
-    var _re3 = /{{{\s*(\w+)\s*}}}/g,
-        _re2 = /{{\s*(\w+)\s*}}/g,
-        _re1 = /[&\"'<>\\]/g,  // "
+    var _no_escape_mustaches = /{{{\s*(\w+)\s*}}}/g,
+        _escape_mustaches = /{{\s*(\w+)\s*}}/g,
+        _escapeables = /[&\"'<>\\]/g,
         _escape_map = {"&": "&amp;", "\\": "&#92;", "\"": "&quot;", "'": "&#39;", "<": "&lt;", ">": "&gt;"},
         _escapeHTML = function(s) {
-            return s.replace(_re1, function(c) { return _escape_map[c]; });
+            return s.replace(_escapeables, function(c) { return _escape_map[c] || c });
         },
+        _val_or_empty = function(val) { return ([undefined, null, false].indexOf(val)>-1) ? "" : val.toString(); },
         pub = {};
     pub.to_html = function (template, data) {
         return template
-            .replace(_re3, function (m, key) { return data[key] || ""; })
-            .replace(_re2, function (m, key) { return _escapeHTML(data[key] || ""); });
+            .replace(_no_escape_mustaches, function (m, key) { return _val_or_empty(data[key]) })
+            .replace(_escape_mustaches, function (m, key) {
+                return _escapeHTML(_val_or_empty(data[key]));
+            });
     };
     return pub;
 })();
